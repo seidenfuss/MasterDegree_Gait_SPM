1bf6a6ddb712c7079eec3cae5c8c693ec1d5c553
Ana Neves <seidenfussbender@gmail.com>
2021-05-22 22:01:25 -0300

    working on Nature GaitRec DB
    retrieving data from HC women with selfselected speed and normal shoes as shod condition; Also separated into age groups: young, adults and older adults;

--- nature_data/women_HC/nature_women_HealthyControls.m
+++ nature_data/women_HC/nature_women_HealthyControls.m
@@ -1,3 +1,52 @@
 clear; 
 load('NATURE_Women_HealthyControls.mat');
- 
+
+
+
+%% shod == 1 (normal shoe) && speed == 2 (selfselected speed)
+j=1;
+for k=1:height(nature_data_W.annotation)
+ if nature_data_W.annotation{k,12}==1 && nature_data_W.annotation{k,14}==2
+     keep(j,1)=k; 
+     j=j+1;
+ end
+end
+meta_nature_W=table2struct(nature_data_W.annotation);
+
+for k=1:length(keep)
+meta_nature_W2(k)=meta_nature_W(keep(k,1));
+end
+
+%% get GRF_R and GRF_L for three components o the force vector;
+
+for k=1:length(meta_nature_W2)
+m=1;
+    for j=1:height(nature_data_W.GRF_F_PRO_AP_R_women)
+        if nature_data_W.GRF_F_PRO_AP_R_women{j,2}==meta_nature_W2(k).SESSION_ID
+            meta_nature_W2(k).GRF_AP_R(m,:)=nature_data_W.GRF_F_PRO_AP_R_women(j,:);
+            meta_nature_W2(k).GRF_V_R(m,:)=nature_data_W.GRF_F_PRO_V_R_women(j,:);
+            meta_nature_W2(k).GRF_ML_R(m,:)=nature_data_W.GRF_F_PRO_ML_R_women(j,:);
+            meta_nature_W2(k).GRF_AP_L(m,:)=nature_data_W.GRF_F_PRO_AP_L_women(j,:);
+            meta_nature_W2(k).GRF_V_L(m,:)=nature_data_W.GRF_F_PRO_V_L_women(j,:);
+            meta_nature_W2(k).GRF_ML_L(m,:)=nature_data_W.GRF_F_PRO_ML_L_women(j,:);
+            m=m+1;
+        end
+    end
+end
+
+%meta_nature_W2=orderfields(meta_nature_W2.AGE);
+
+% suppose 's' is the struct array and you want to sort it by the values in field 'f_sortby'
+[x,idx]=sort([meta_nature_W2.AGE]);
+meta_nature_W2=meta_nature_W2(idx);
+   
+%% separate by age groups
+
+%young(15-25)
+NATURE_women.young=meta_nature_W2(1:29);
+%adults(26-45)
+NATURE_women.adult=meta_nature_W2(30:77);
+%older_adults(46-)
+NATURE_women.olderAdult=meta_nature_W2(78:105);
+
+save('NATURE_women.mat','NATURE_women')
---
+++ v.0.1/4_SPM1D_NATURE/HotellingT2_young_adult_olderAdult.m
@@ -0,0 +1,28 @@
+%%load datasets - mean between R and left??
+
+%Y_young
+
+%Y_adult
+
+%Y_olderAdult
+
+
+
+
+clear;  clc
+%(0) Load data:
+dataset    = spm1d.data.mv1d.hotellings2.Besier2009muscleforces();
+[YA,YB]    = deal(dataset.YA, dataset.YB);
+
+
+%(1) Conduct SPM analysis:
+spm       = spm1d.stats.hotellings2(YA, YB);
+spmi      = spm.inference(0.05);
+disp(spmi)
+
+
+%(2) Plot:
+close all
+spmi.plot();
+spmi.plot_threshold_label();
+spmi.plot_p_values();
